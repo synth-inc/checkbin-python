3c838d11bfd1da333b9b0002692d868f512c5b97
Jay Swanson <jay@synth.inc>
2024-10-08 11:21:48 -0700

    Prototype local checkbin server

--- src/checkbin/server.py
+++ src/checkbin/server.py
@@ -1,8 +1,288 @@
-from fastapi import FastAPI
+import uuid
+from typing import Literal, Optional
+from pathlib import Path
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from tinydb import TinyDB, Query
 
 app = FastAPI()
+current_dir = Path(__file__).parent
+db_dir = Path(current_dir, "client_db")
+db_dir.mkdir(exist_ok=True)
+db = TinyDB(db_dir / "db.json")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Checkin
+class CheckinFile(BaseModel):
+    url: str
+    mediaType: Optional[Literal["image", "video"]] = None
+    pickle: Optional[bool] = None
+
+
+class Checkin(BaseModel):
+    name: Optional[str] = None
+    ids: Optional[dict[str, str | float | int]] = None
+    state: Optional[dict] = None
+    files: Optional[dict[str, CheckinFile]] = None
+
+
+def create_checkin(
+    checkin: Checkin,
+    is_output: bool,
+    run_id: Optional[str] = None,
+    parent_id: Optional[str] = None,
+):
+    checkin_table = db.table("checkin")
+    checkin_state_table = db.table("checkinState")
+    new_checkin = {
+        "id": str(uuid.uuid4()),
+        "runId": run_id,
+        "parentId": parent_id,
+        "name": checkin.name,
+        "ids": checkin.ids,
+        "isOutput": is_output,
+    }
+    checkin_table.insert(new_checkin)
+    state_list = []
+    if checkin.state is not None:
+        for key, value in checkin.state.items():
+            state_list.append(
+                {
+                    "id": str(uuid.uuid4()),
+                    "checkinId": new_checkin["id"],
+                    "name": key,
+                    "data": value,
+                }
+            )
+    if checkin.files is not None:
+        for key, file in checkin.files.items():
+            state = {
+                "id": str(uuid.uuid4()),
+                "checkinId": new_checkin["id"],
+                "name": key,
+            }
+            state.update(file)
+            state_list.append(state)
+    for state in state_list:
+        checkin_state_table.insert(state)
+    return new_checkin
+
+
+def get_checkin_by_id(checkin_id: str, include_state: bool):
+    checkin_table = db.table("checkin")
+    checkin = checkin_table.search(Query().id == checkin_id)
+    if len(checkin) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"No checkin found with id {checkin_id}"
+        )
+    checkin = checkin[0].copy()
+    if include_state:
+        checkin_state_table = db.table("checkinState")
+        state = checkin_state_table.search(Query().checkinId == checkin_id)
+        checkin["state"] = state
+    return checkin
+
+
+@app.get("/checkin/{checkinId}")
+def get_checkin(checkinId: str, includeState: bool = False):
+    return get_checkin_by_id(checkinId, includeState)
+
+
+class PostCheckins(BaseModel):
+    runId: str
+    checkins: list[Checkin]
+    parentId: Optional[str] = None
+
+
+@app.post("/checkin")
+def create_checkins(body: PostCheckins):
+    parent_id = body.parentId
+    for index, checkin in enumerate(body.checkins):
+        new_checkin = create_checkin(
+            checkin, index == len(body.checkins) - 1, body.runId, parent_id
+        )
+        parent_id = new_checkin["id"]
+
+
+# Set
+@app.get("/set")
+def get_sets():
+    set_table = db.table("set")
+    return set_table.all()
+
+
+def get_set_by_id(
+    set_id: str, include_checkins: bool = False, include_state: bool = False
+):
+    set_table = db.table("set")
+    set = set_table.search(Query().id == set_id)
+    if len(set) == 0:
+        raise HTTPException(status_code=404, detail=f"No set found with id {set_id}")
+    set = set[0].copy()
+    if include_checkins:
+        set_checkin_table = db.table("setCheckin")
+        set_checkins = set_checkin_table.search(Query().setId == set_id)
+        checkin_ids = [set_checkin["checkinId"] for set_checkin in set_checkins]
+        checkin_table = db.table("checkin")
+        checkins = checkin_table.search(Query().id.one_of(checkin_ids))
+        checkins = [checkin.copy() for checkin in checkins]
+        set["checkins"] = checkins
+        if include_state:
+            for checkin in checkins:
+                checkin_state_table = db.table("checkinState")
+                state = checkin_state_table.search(Query().checkinId == checkin["id"])
+                checkin["state"] = state
+    return set
+
+
+@app.get("/set/{setId}")
+def get_set(setId: str, includeCheckins: bool = False, includeState: bool = False):
+    return get_set_by_id(setId, includeCheckins, includeState)
+
+
+class PostSet(BaseModel):
+    name: str
+    isInput: bool
+    checkins: list[Checkin]
+
+
+def add_checkin_to_set(set_id: str, checkin_id: str):
+    set_checkin_table = db.table("setCheckin")
+    set_checkin_table.insert(
+        {
+            "setId": set_id,
+            "checkinId": checkin_id,
+        }
+    )
+
+
+@app.post("/set")
+def create_set(body: PostSet):
+    set_table = db.table("set")
+    set_id = str(uuid.uuid4())
+    new_set = {"id": set_id, "name": body.name, "isInput": body.isInput}
+    set_table.insert(new_set)
+    for checkin in body.checkins:
+        checkin.name = "Input"
+        new_checkin = create_checkin(checkin, False)
+        add_checkin_to_set(set_id, new_checkin["id"])
+    return new_set
+
+
+# Run
+@app.get("/run")
+def get_runs():
+    run_table = db.table("run")
+    return run_table.all()
+
+
+def get_checkin_ancestors(checkins: list[dict], include_state: bool):
+    checkin_table = db.table("checkin")
+    tests = []
+    for output_checkin in checkins:
+        test = []
+        current_checkin = output_checkin
+        while current_checkin is not None:
+            if include_state:
+                checkin_state_table = db.table("checkinState")
+                state = checkin_state_table.search(
+                    Query().checkinId == current_checkin["id"]
+                )
+                current_checkin["state"] = state
+            test.insert(0, current_checkin)
+            parent_checkin = checkin_table.search(
+                Query().id == current_checkin["parentId"]
+            )
+            if len(parent_checkin) == 0:
+                current_checkin = None
+            else:
+                parent_checkin = parent_checkin[0].copy()
+                current_checkin = parent_checkin
+        tests.append(test)
+    return tests
+
+
+def get_run_by_id(
+    run_id: str, include_checkins: bool = False, include_state: bool = False
+):
+    run_table = db.table("run")
+    run = run_table.search(Query().id == run_id)
+    if len(run) == 0:
+        raise HTTPException(status_code=404, detail=f"No run found with id {run_id}")
+    run = run[0].copy()
+    if include_checkins:
+        checkin_table = db.table("checkin")
+        get_query = Query()
+        checkins = checkin_table.search(
+            get_query.runId == run_id and get_query.isOutput == True
+        )
+        checkins = [checkin.copy() for checkin in checkins]
+        run["checkins"] = get_checkin_ancestors(checkins, include_state)
+    return run
+
+
+@app.get("/run/{runId}")
+def get_run(runId: str, includeCheckins: bool = False, includeState: bool = False):
+    return get_run_by_id(runId, includeCheckins, includeState)
+
+
+def get_run_count():
+    run_table = db.table("run")
+    return len(run_table.all())
+
+
+class PostRun(BaseModel):
+    name: Optional[str] = None
+
+
+@app.post("/run")
+def create_run(body: PostRun):
+    run_table = db.table("run")
+    run_id = str(uuid.uuid4())
+    name = body.name
+    if name is None:
+        name = f"Run {get_run_count() + 1}"
+    new_run = {"id": run_id, "name": name}
+    run_table.insert(new_run)
+    return new_run
+
+
+@app.get("/run/{runId}/testStatus")
+def get_test_status(runId: str):
+    test_status_table = db.table("testStatus")
+    return test_status_table.search(Query().runId == runId)
+
+
+class TestStatus(BaseModel):
+    checkinId: str
+    value: Literal["pending", "running", "completed", "failed"] = "pending"
+
+
+class PatchTestStatuses(BaseModel):
+    testStatuses: list[TestStatus]
+
+
+@app.patch("/run/{runId}/testStatus")
+def update_test_status(runId: str, body: PatchTestStatuses):
+    test_status_table = db.table("testStatus")
+    for test_status in body.testStatuses:
+        read_query = Query()
+        current_test_status = test_status_table.search(
+            read_query.runId == runId and read_query.checkinId == test_status.checkinId
+        )
+        if len(current_test_status) == 0:
+            test_status_table.insert(
+                {
+                    "runId": runId,
+                    "checkinId": test_status.checkinId,
+                    "value": test_status.value,
+                }
+            )
+        else:
+            update_query = Query()
+            test_status_table.update(
+                {"value": test_status.value},
+                update_query.runId == runId
+                and read_query.checkinId == test_status.checkinId,
+            )
